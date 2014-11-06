9bc42ba0e827881962cd62ca59a6cd13d443dd59
Darrell De Freitas <darrell.defreitas@gmail.com>
2014-11-06 15:58:04 -0500

    .properties file is now created on the fly

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Date;
+import java.util.Scanner;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.PropertiesCredentials;
@@ -11,14 +16,34 @@ import com.amazonaws.services.glacier.transfer.UploadResult;
 public class Main {
 
     public static String vaultName = "MtgWorks";
-    public static String archiveToUpload = "C:\\Users\\ddefreitas\\Downloads\\Mtgworks-rackspace-9-12-14.sql";
+    public static String archiveToUpload = "C:\\Users\\ddefreitas\\Downloads\\test.txt";
     
     public static AmazonGlacierClient client;
+	private static Scanner scanner;
     
     public static void main(String[] args) throws IOException {
         
-        AWSCredentials credentials = new PropertiesCredentials(
-                Main.class.getResourceAsStream("AwsCredentials.properties"));
+        File f = new File(System.getProperty("user.home") + "/MyFile.properties");
+        if(!f.exists())
+        {
+        	scanner = new Scanner(System.in);
+        	System.out.print("secretKey = ");
+        	String secretKey = scanner.nextLine();
+        	System.out.print("accessKey = ");
+        	String accessKey = scanner.nextLine();
+        	FileWriter output = new FileWriter(System.getProperty("user.home") + "/MyFile.properties");
+        	
+        	output.write("secretKey=");
+        	output.write(secretKey);
+        	output.write("\r\n");
+        	output.write("accessKey=");
+        	output.write(accessKey);
+        	
+        	output.close();
+        	
+        	
+        }
+        AWSCredentials credentials = new PropertiesCredentials(new File(System.getProperty("user.home") + "/MyFile.properties"));
         client = new AmazonGlacierClient(credentials);
         client.setEndpoint("https://glacier.us-west-2.amazonaws.com/");
 
