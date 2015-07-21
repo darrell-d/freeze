24e4627bda588507706f0e6b2ea3bb753b99d1e2
Darrell De Freitas <darrell.defreitas@gmail.com>
2015-07-21 12:40:12 -0400

    GUI Layout

--- com/darrelld/freeze/GUI.java
+++ com/darrelld/freeze/GUI.java
@@ -1,16 +1,33 @@
 package com.darrelld.freeze;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.ScrollPane;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import java.awt.Window.Type;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTree;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
 public class GUI {
 
 	private JFrame frmFreeze;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -41,7 +58,7 @@ public class GUI {
 	private void initialize() {
 		frmFreeze = new JFrame();
 		frmFreeze.setTitle("Freeze");
-		frmFreeze.setBounds(100, 100, 850, 850);
+		frmFreeze.setBounds(100, 100, 850, 766);
 		frmFreeze.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JMenuBar menuBar = new JMenuBar();
@@ -52,6 +69,114 @@ public class GUI {
 		
 		JMenuItem mntmManageAccount = new JMenuItem("Manage Account");
 		mnAccount.add(mntmManageAccount);
+		
+		JLabel lblRegions = new JLabel("Regions");
+		lblRegions.setFont(new Font("Arial", Font.BOLD, 14));
+		
+		JSplitPane splitPane = new JSplitPane();
+		
+		JButton btnNewButton = new JButton("Upload");
+		
+		JButton btnNewButton_1 = new JButton("Download");
+		
+		JButton btnDelete = new JButton("Delete");
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		GroupLayout groupLayout = new GroupLayout(frmFreeze.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(tabbedPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 814, Short.MAX_VALUE)
+						.addComponent(splitPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 814, Short.MAX_VALUE)
+						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+							.addComponent(lblRegions)
+							.addGap(67)
+							.addComponent(btnNewButton)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnNewButton_1)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnDelete)))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblRegions)
+						.addComponent(btnNewButton)
+						.addComponent(btnNewButton_1)
+						.addComponent(btnDelete))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(splitPane, GroupLayout.PREFERRED_SIZE, 245, GroupLayout.PREFERRED_SIZE)
+					.addGap(28)
+					.addComponent(tabbedPane, GroupLayout.PREFERRED_SIZE, 372, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(101, Short.MAX_VALUE))
+		);
+		
+		ScrollPane scrollPane_2 = new ScrollPane();
+		tabbedPane.addTab("Info", null, scrollPane_2, null);
+		
+		ScrollPane scrollPane_3 = new ScrollPane();
+		tabbedPane.addTab("Log", null, scrollPane_3, null);
+		
+		ScrollPane scrollPane_1 = new ScrollPane();
+		tabbedPane.addTab("Actions", null, scrollPane_1, null);
+		
+		JTree tree = new JTree();
+		splitPane.setLeftComponent(tree);
+		tree.setModel(new DefaultTreeModel(
+			new DefaultMutableTreeNode("All Regions") {
+				{
+					DefaultMutableTreeNode node_1;
+					node_1 = new DefaultMutableTreeNode("US East");
+						node_1.add(new DefaultMutableTreeNode("-"));
+					add(node_1);
+					node_1 = new DefaultMutableTreeNode("US West 1");
+						node_1.add(new DefaultMutableTreeNode("-"));
+					add(node_1);
+					node_1 = new DefaultMutableTreeNode("US West 2");
+						node_1.add(new DefaultMutableTreeNode("-"));
+					add(node_1);
+					node_1 = new DefaultMutableTreeNode("EU West");
+						node_1.add(new DefaultMutableTreeNode("-"));
+					add(node_1);
+					node_1 = new DefaultMutableTreeNode("AP Southeast 1");
+						node_1.add(new DefaultMutableTreeNode("-"));
+					add(node_1);
+					node_1 = new DefaultMutableTreeNode("AP Southeast 2");
+						node_1.add(new DefaultMutableTreeNode("-"));
+					add(node_1);
+					node_1 = new DefaultMutableTreeNode("AP Northeast 1");
+						node_1.add(new DefaultMutableTreeNode("-"));
+					add(node_1);
+					node_1 = new DefaultMutableTreeNode("SA East");
+						node_1.add(new DefaultMutableTreeNode("-"));
+					add(node_1);
+				}
+			}
+		));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		splitPane.setRightComponent(scrollPane);
+		
+		table = new JTable();
+		table.setBorder(null);
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Test", "ZIP", "2MB", "10/24/2015"},
+				{"Important", "DOCX", "1MB", "7/04/2014"},
+			},
+			new String[] {
+				 "Filename", "Type", "Size", "Date modified"
+			}
+		));
+		table.getColumnModel().getColumn(1).setPreferredWidth(113);
+		table.getColumnModel().getColumn(3).setPreferredWidth(183);
+		frmFreeze.getContentPane().setLayout(groupLayout);
 	}
 
 	public void setVisible(boolean b) {
@@ -62,5 +187,4 @@ public class GUI {
 	{
 		return frmFreeze.isVisible();
 	}
-
 }
